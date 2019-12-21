eb144893b57447716b8e3ece143422889906c337
john2796 <jbmiranda22796@gmail.com>
2019-12-21 14:09:12 -0800

    cleaning up files

--- data/migrations/20190325200130_users.js
+++ data/migrations/20190325200130_users.js
@@ -1,24 +1,17 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable("users", tbl => {
-    tbl.increments();
-    tbl.string("name");
-    tbl.string("email");
-    tbl.string("image_url");
-    tbl.string("nickname");
-    tbl.string("sub");
-    tbl.string("acct_type");
-    tbl.string("phone");
-    tbl.float("amount").defaultTo(0);
-    tbl.boolean("show_tour").defaultTo(true);
-    tbl.boolean("show_ad_tour").defaultTo(true);
+    tbl.increments()
+    tbl.string("firstName")
+    tbl.string("lastName")
     tbl
-      .string("stripe_cust_id", 128)
-      .nullable()
-      .unique();
-    tbl.string("stripe_payout_id", 128).nullable();
-  });
-};
+      .string("email")
+      .unique()
+      .notNullable()
+    tbl.string("password").notNullable()
+    tbl.boolean("isDeleted").defaultTo(false)
+  })
+}
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTableIfExists("users");
-};
+  return knex.schema.dropTableIfExists("users")
+}
--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-require("dotenv").config();
-
-const server = require("./api/server");
+require("dotenv").config()
+const server = require("./api/server")
 // 404
-server.use(function (req, res, next) {
+server.use(function(req, res, next) {
   return res
     .status(404)
-    .send({ message: "[Route] --> " + req.url + " <-- Not found." });
-});
+    .send({ message: "[Route] --> " + req.url + " <-- Not found." })
+})
 // 500 - Any server error
-server.use(function (err, req, res, next) {
-  return res.status(500).json({ error: err });
-});
+server.use(function(err, req, res, next) {
+  return res.status(500).json({ error: err })
+})
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000
 
-server.listen(port, () => console.log(`🔥 -------- listening on port ${port} ---------- 🔥`));
+server.listen(port, () =>
+  console.log(`🔥 -------- listening on port ${port} ---------- 🔥`)
+)
